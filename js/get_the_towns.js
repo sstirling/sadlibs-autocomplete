0d2cdd089987a6438706769ee0f8946f1f9ec82c
sstirling <stephenstirling@gmail.com>
2017-12-03 22:41:13 -0500

    Added sadlib text. Chart img still fuzzy.

--- js/get_the_towns.js
+++ js/get_the_towns.js
@@ -6,7 +6,7 @@ var results_json = [];
 
 $(document).ready(function () {
 
-$.getJSON( 'js/muni-co-test.json', function( data ) {
+$.getJSON( 'js/muni-co-working.json', function( data ) {
   results_json.push(data)
   pymChild.sendHeight();
 });
@@ -49,18 +49,25 @@ $("#input_click" ).click(function() {
 
 function create_cards_house(data,town_val) {
   key = data[town_val][0]
+  //  $('.district').html(
+  //   $("<div class='img-pic'><img src='county-img/" +key.coimg+ "'></div>")
+  // );
   $('.district').html(
-    $("<div class='title'>Municipality: "+key.Ledgerstyle+"</div>")
+    $("<div class='legislators1'>"+key.Ledgerstyle+"</div>")
   );
   $('.house').html(
-    $("<div class='chamber'>"+key.County+" County</div>")
+    $("<div class='legislators1'>"+key.County+" County</div>" )
   );
  $('.house').append(
-    $("<div class='legislators'><p>You live in <span class='robotext'>"+ key.County + "</span> County. If you die under suspicious or strange circumstances, the <span class='robotext'>" + key.Ledgerstyle + "</span> office will be tasked with investigating your case.<br><br>In 2016, it conducted <span class='robotext'>" + key.Autopsy + "</span> autopsies, about <span class='robotext'>" + key.perautopsy + "%</span> of the cases they handled.</div>")
+    $("<div class='legislators'><p>If you die under suspicious or strange circumstances, the <span class='robotext'>" + key.office + " Medical Examiner's Office</span> will be tasked with investigating your case.<br><br><p>This office completes <span class='robotext'>" + key.u60per + "% </span>of its cases in under 60 days, which <span class='robotext'>" + key.accred + " </span>make it eligible for accreditation from the National Association of Medical Examiners.</div>")
   );
   $('.house').append(
-    $("<div class='legislators'><div class='name'>" + key.Assembly_2 + "</div><div class='contact-info'><table class='geo-table'><tr><td>Phone:</td><td><a href='tel:1-"+ key.Assembly_phone_1 + "'>" + key.Assembly_phone_2 + "</a></td></tr><tr><td> Email:</td><td><a href='mailto:" + key.Assembly_email_2 + "'>" + key.Assembly_email_2+"</a></td></tr><tr><td>Address:</td><td>"+ key.Assembly_office_2 +"</table></div></div>")
+    $("<div class='legislators'><p>In 2016, the <span class='robotext'>" + key.office + " </span>office tossed out <span class='robotext'>" + key.per_released + "%</span> of its cases without ever viewing the body, which is <span class='robotext'>" +key.release_state+ " </span>than the state average of <span class='robotext'>" +key.nj_per_released + "% </span>.<br><br><p>This office completed <span class='robotext'>" + key.autopsy + "</span> autopsies, about <span class='robotext'>" + key.per_autopsy + "%</span> of the cases they handled. This is <span class='robotext'>" + key.auto_days_hl + " </span>than the state average of <span class='robotext'>" + key.nj_per_auto + "%</span>. <img src='chart-img/" + key.chart +"' width='100%'></div>")
   );
+
+  // $('.house').append(
+  //   $("<div class='legislators'><div class='name'>" + key.Assembly_2 + "</div><div class='contact-info'><table class='geo-table'><tr><td>Phone:</td><td><a href='tel:1-"+ key.Assembly_phone_1 + "'>" + key.Assembly_phone_2 + "</a></td></tr><tr><td> Email:</td><td><a href='mailto:" + key.Assembly_email_2 + "'>" + key.Assembly_email_2+"</a></td></tr><tr><td>Address:</td><td>"+ key.Assembly_office_2 +"</table></div></div>")
+  // );
   pymChild.sendHeight();
 };
 
