44c4b8406e2190b9257552a6782e4a263b1931a9
sstirling <stephenstirling@gmail.com>
2017-12-08 19:34:16 -0500

    Changed style to enlarge text on desktop. Otherwise, good to go.

--- js/get_the_towns.js
+++ js/get_the_towns.js
@@ -53,95 +53,22 @@ function create_cards_house(data,town_val) {
   //   $("<div class='img-pic'><img src='county-img/" +key.coimg+ "'></div>")
   // );
   $('.district').html(
-    $("<div class='legislators1'>"+key.Ledgerstyle+"</div>")
+    $("<div class='sadlib-block1'>"+key.Ledgerstyle+"</div>")
   );
   $('.house').html(
-    $("<div class='legislators1'>"+key.County+" County</div>" )
+    $("<div class='sadlib-block1'>"+key.County+" County</div>" )
   );
  $('.house').append(
-    $("<div class='legislators'><p>If you die under suspicious or strange circumstances, the <span class='robotext'>" + key.office + " Medical Examiner's Office</span> will be tasked with investigating your case.<br><p>In <span class='robotext'>" + key.year + "</span>, the most recent data available, this office completed <span class='robotext'>" + key.u60per + "% </span>of its cases in under 60 days, which <span class='robotext'>" + key.accred + " </span>make it eligible for accreditation from the National Association of Medical Examiners.<br><img src='accred-img/" + key.chart +"' width='100%'></div>")
+    $("<div class='sadlib-block'><p>If you die under suspicious or strange circumstances, the <span class='robotext'>" + key.office + " Medical Examiner's Office</span> will be tasked with investigating your case.<br><p>In <span class='robotext'>" + key.year + "</span>, the most recent data available, this office completed <span class='robotext'>" + key.u60per + "% </span>of its cases in under 60 days, which <span class='robotext'>" + key.accred + " </span>make it eligible for accreditation from the National Association of Medical Examiners.<br><img src='accred-img/" + key.chart +"' width='100%'></div>")
   );
   $('.house').append(
-    $("<div class='legislators'><p>In 2016, the <span class='robotext'>" + key.office + " </span>office tossed out <span class='robotext'>" + key.per_released + "%</span> of its cases without ever viewing the body, which is <span class='robotext'>" +key.release_state_ml+ " </span>than the state average of <span class='robotext'>" +key.nj_per_released + "% </span>.<br><p>This office completed <span class='robotext'>" + key.autopsy + "</span> autopsies, about <span class='robotext'>" + key.per_autopsy + "%</span> of the cases they handled. This is <span class='robotext'>" + key.auto_days_hl + " </span>than the state average of <span class='robotext'>" + key.nj_per_auto + "%</span>.<br><p>In <span class='robotext'>" + key.year + "</span> it took this office <span class='robotext'>" + key.days_overall + "</span> days to complete an average case, which is <span class='robotext'>" + key.days_hl + "</span> than the state average of <span class='robotext'>" + key.days_state + "</span>. <img src='chart-img/" + key.chart +"' width='100%'></div>")
+    $("<div class='sadlib-block'><p>In 2016, the <span class='robotext'>" + key.office + " </span>office tossed out <span class='robotext'>" + key.per_released + "%</span> of its cases without ever viewing the body, which is <span class='robotext'>" +key.release_state_ml+ " </span>than the state average of <span class='robotext'>" +key.nj_per_released + "% </span>.<br><p>This office completed <span class='robotext'>" + key.autopsy + "</span> autopsies, about <span class='robotext'>" + key.per_autopsy + "%</span> of the cases they handled. This is <span class='robotext'>" + key.auto_days_hl + " </span>than the state average of <span class='robotext'>" + key.nj_per_auto + "%</span>.<br><p>In <span class='robotext'>" + key.year + "</span> it took this office <span class='robotext'>" + key.days_overall + "</span> days to complete an average case, which is <span class='robotext'>" + key.days_hl + "</span> than the state average of <span class='robotext'>" + key.days_state + "</span>. <img src='chart-img/" + key.chart +"' width='100%'></div>")
   );
 
-  // $('.house').append(
-  //   $("<div class='legislators'><div class='name'>" + key.Assembly_2 + "</div><div class='contact-info'><table class='geo-table'><tr><td>Phone:</td><td><a href='tel:1-"+ key.Assembly_phone_1 + "'>" + key.Assembly_phone_2 + "</a></td></tr><tr><td> Email:</td><td><a href='mailto:" + key.Assembly_email_2 + "'>" + key.Assembly_email_2+"</a></td></tr><tr><td>Address:</td><td>"+ key.Assembly_office_2 +"</table></div></div>")
-  // );
+
   pymChild.sendHeight();
 };
 
-// function create_cards_senate(data,town_val) {
-//   key = data[town_val][0]
-//   $('.senate').html(
-//     $("<div class='chamber'>SENATE</div>")
-//   );
-//  $('.senate').append(
-//       $("<div class='legislators'><div class='name'>" + key.Senate + "</div><div class='contact-info'><table class='geo-table'><tr><td>Phone:</td><td><a href='tel:1-"+ key.Senate_phone + "'>" + key.Senate_phone + "</a></td></tr><tr><td> Email:</td><td><a href='mailto:" + key.Senate_email + "'>" + key.Senate_email+"</a></td></tr><tr><td>Address:</td><td>"+ key.Senate_main_office +"</table></div></div>")
-//   );
-//   // $('#close-geo').show();
-//   pymChild.sendHeight();
-// };
-
-function create_house_split(data,town_val) {
-  key=data[town_val];
-  console.log(key)
-    $('.house').html(
-      $("<div></div>")
-    );
-      $('.district').html(
-      $("<div></div>")
-    );
-    //  $('.senate').html(
-    //   $("<div></div>")
-    // );
-  $.each(key,function(i,item){
-
-    $('.house').append(
-    $("<div class='district title'>DISTRICT "+key[i].County+"</div><div class='chamber'>ASSEMBLY</div>")
-  );
-   $('.house').append(
-      $("<div class='legislators'><div class='name'>" + key[i].Autopsy + "</div><div class='contact-info'><table class='geo-table'><tr><td>Phone:</td><td><a href='tel:1-"+ key[i].Assembly_phone_1 + "'>" + key[i].Assembly_phone_1 + "</a></td></tr><tr><td> Email:</td><td><a href='mailto:" + key[i].Assembly_email_1 + "'>" + key[i].Assembly_email_1+"</a></td></tr><tr><td>Address:</td><td>"+ key[i].Assembly_office_1 +"</table></div></div>")
-    );
-    $('.house').append(
-      $("<div class='legislators'><div class='name'>" + key[i].Assembly_2 + "</div><div class='contact-info'><table class='geo-table'><tr><td>Phone:</td><td><a href='tel:1-"+ key[i].Assembly_phone_1 + "'>" + key[i].Assembly_phone_2 + "</a></td></tr><tr><td> Email:</td><td><a href='mailto:" + key[i].Assembly_email_2 + "'>" + key[i].Assembly_email_2+"</a></td></tr><tr><td>Address:</td><td>"+ key[i].Assembly_office_2 +"</table></div></div>")
-    );
-    $('.house').append(
-    $("<div class='chamber'>SENATE</div>")
-    );
-    $('.house').append(
-      $("<div class='legislators'><div class='name'>" + key[i].Senate + "</div><div class='contact-info'><table class='geo-table'><tr><td>Phone:</td><td><a href='tel:1-"+ key[i].Senate_phone + "'>" + key[i].Senate_phone + "</a></td></tr><tr><td> Email:</td><td><a href='mailto:" + key[i].Senate_email + "'>" + key[i].Senate_email+"</a></td></tr><tr><td>Address:</td><td>"+ key[i].Senate_main_office +"</table></div></div>")
-      );
-  });
-  pymChild.sendHeight();
-};
-
-
-// function create_sen_split(data,town_val) {
-//   key=data[town_val];
-//     // $('.senate').html(
-//     //   $("<div></div>")
-//     // );
-//   $.each(key,function(i,item){
-
-//     $('.house').append(
-//     $("<div class='district title'>"+key[i].County+"</div><div class='chamber'>SENATE</div>")
-//   );
-//    $('.house').append(
-//       $("<div class='legislators'><div class='name'>" + key[i].Senate + " ("+ key[i].Senate_party+")</div><div class='contact-info'><table class='geo-table'><tr><td>Phone:</td><td><a href='tel:1-"+ key[i].Senate_phone + "'>" + key[i].Senate_phone + "</a></td></tr><tr><td> Email:</td><td><a href='mailto:" + key[i].Senate_email + "'>" + key[i].Senate_email+"</a></td></tr><tr><td>Address:</td><td>"+ key[i].Senate_main_office +"</table></div></div>")
-//       );
-//   });
-//   pymChild.sendHeight();
-// };
-
-
-
-
-  // $('#close-geo').click(function(){
-  //   console.log("this")
-  //   $('.house').slideToggle(function(){pymChild.sendHeight();});
-  //   $('.senate').slideToggle(function(){pymChild.sendHeight();});
-  // });
 
 
 });
